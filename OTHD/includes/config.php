60ebaaabb305b792d6ebcd864397f518c63c3bc5
Rushik Ramani <rush.ramani@gmail.com>
2022-04-22 19:20:41 -0700

    db name changed\

--- OTHD/includes/config.php
+++ OTHD/includes/config.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "hoofman");
+$con=mysqli_connect("localhost", "root", "", "dfsms");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
