15d65a75ba709e88fa3636965202784ed91e07d1
csuftitan <csuftitan@MacBook-Air-7.lan>
2022-04-22 17:48:07 -0700

    added

--- OTHD/includes/config.php
+++ OTHD/includes/config.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "dfsms");
+$con=mysqli_connect("localhost", "root", "", "hoofman");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
